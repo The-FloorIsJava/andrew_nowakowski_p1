385fadbf5c01133c6185408d423f657b60f63ea4
Andrew Nowakowski <64051143+andrewmnowakowski@users.noreply.github.com>
2022-11-23 10:29:24 -0800

    Updated tests in order to include mocking and added a slight change in the user controller to reject invalid users from requests.

--- src/test/java/com/revature/ReimbursementSystem/Service/UserServiceTestSuite.java
+++ src/test/java/com/revature/ReimbursementSystem/Service/UserServiceTestSuite.java
@@ -2,94 +2,119 @@ package com.revature.ReimbursementSystem.Service;
 
 import com.revature.ReimbursementSystem.DAO.UserDAO;
 import com.revature.ReimbursementSystem.Model.User;
+import com.revature.ReimbursementSystem.Util.DTO.LoginCredentials;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 public class UserServiceTestSuite {
 
-    private UserService userService;
+    UserService sut;
+    UserDAO mockUserDAO;
 
-//    @Before
-//    public void setUp() {
-//        userService = new UserService(new UserDAO());
-//    }
-//
-//    @Test
-//    public void test_registerUser_returnTrue_givenNoUsersExist() {
-//        User newUser = new User("test", "123");
-//
-//        boolean actual = userService.registerUser(newUser);
-//        User[] expected = {newUser};
-//
-//        Assert.assertTrue(actual);
-//        Assert.assertArrayEquals(expected, userService.getAllUsers().toArray());
-//    }
-//
-//    @Test
-//    public void test_registerUser_returnFalse_givenUserIsInvalid() {
-//        User newUser = new User();
-//
-//        boolean actual = userService.registerUser(newUser);
-//        User[] expected = {};
-//
-//        Assert.assertFalse(actual);
-//        Assert.assertArrayEquals(expected, userService.getAllUsers().toArray());
-//    }
-//
-//    @Test
-//    public void test_registerUser_returnFalse_givenUsernameIsTaken() {
-//        User newUser = new User("test", "123");
-//        User oldUser = new User("test", "456");
-//
-//        userService.addUser(oldUser);
-//        boolean actual = userService.registerUser(newUser);
-//        User[] expected = {oldUser};
-//
-//        Assert.assertFalse(actual);
-//        Assert.assertArrayEquals(expected, userService.getAllUsers().toArray());
-//    }
-//
-//    @Test
-//    public void test_registerUser_returnTrue_givenUsernameIsNotTaken() {
-//        User newUser = new User("test", "123");
-//        User oldUser = new User("notTest", "5565");
-//
-//        userService.registerUser(oldUser);
-//        boolean actual = userService.registerUser(newUser);
-//        User[] expected = {oldUser, newUser};
-//
-//        Assert.assertTrue(actual);
-//        Assert.assertArrayEquals(expected, userService.getAllUsers().toArray());
-//    }
-//
-//    @Test
-//    public void test_loginUser_returnTrue_givenCredentialAreCorrect() {
-//        User newUser = new User("test", "123");
-//
-//        userService.registerUser(newUser);
-//        boolean actual = userService.loginUser(newUser);
-//
-//        Assert.assertTrue(actual);
-//    }
-//
-//    @Test
-//    public void test_loginUser_returnFalse_givenCredentialAreIncorrect() {
-//        User user = new User("test", "123");
-//
-//        userService.registerUser(user);
-//        boolean actual = userService.loginUser(new User("test", "124"));
-//
-//        Assert.assertFalse(actual);
-//    }
-//
-//    @Test
-//    public void test_loginUser_returnFalse_givenUserIsNotRegistered() {
-//        User user = new User("test", "123");
-//
-//        boolean actual = userService.loginUser(user);
-//
-//        Assert.assertFalse(actual);
-//    }
+    @Before
+    public void setUp() {
+        mockUserDAO = mock(UserDAO.class);
+        sut = new UserService(mockUserDAO);
+    }
 
+    @Test
+    public void test_registerUser_returnUser_givenValidUser() {
+        // Arrange
+        User newUser = new User("test", "123");
+
+        // Act
+        when(mockUserDAO.insert(newUser)).thenReturn(newUser);
+        User actual = sut.registerUser(newUser);
+
+        // Assert
+        verify(mockUserDAO, times(1)).insert(newUser);
+        Assert.assertNotNull(actual);
+    }
+
+    @Test
+    public void test_registerUser_returnNull_givenInvalidUser() {
+        // Arrange
+        User newUser = new User();
+
+        // Act
+        when(mockUserDAO.insert(newUser)).thenReturn(null);
+        User actual = sut.registerUser(newUser);
+
+        // Assert
+        verify(mockUserDAO, times(1)).insert(newUser);
+        Assert.assertNull(actual);
+    }
+
+    @Test
+    public void test_registerUser_returnNull_givenUsernameIsTaken() {
+        // Arrange
+        User newUser = new User("test", "123");
+        User oldUser = new User("test", "456");
+
+        // Act
+        when(mockUserDAO.insert(oldUser)).thenReturn(oldUser);
+        when(mockUserDAO.insert(newUser)).thenReturn(null);
+        sut.registerUser(oldUser);
+        User actual = sut.registerUser(newUser);
+
+        // Assert
+        verify(mockUserDAO, times(1)).insert(oldUser);
+        verify(mockUserDAO, times(1)).insert(newUser);
+        Assert.assertNull(actual);
+    }
+
+    @Test
+    public void test_registerUser_returnUser_givenUsernameIsNotTaken() {
+        // Arrange
+        User newUser = new User("test", "123");
+        User oldUser = new User("notTest", "456");
+
+        // Act
+        when(mockUserDAO.insert(oldUser)).thenReturn(oldUser);
+        when(mockUserDAO.insert(newUser)).thenReturn(newUser);
+        sut.registerUser(oldUser);
+        User actual = sut.registerUser(newUser);
+
+        // Assert
+        verify(mockUserDAO, times(1)).insert(oldUser);
+        verify(mockUserDAO, times(1)).insert(newUser);
+        Assert.assertNotNull(actual);
+    }
+
+    @Test
+    public void test_loginUser_returnUser_givenCredentialAreCorrect() {
+        // Arrange
+        User newUser = new User("test", "123");
+        LoginCredentials userCred = new LoginCredentials();
+        userCred.setUsername("test");
+        userCred.setPassword("123");
+
+        // Act
+        when(mockUserDAO.loginCheck(userCred.getUsername(), userCred.getPassword())).thenReturn(newUser);
+        User actual = sut.loginUser(userCred);
+
+        //Assert
+        verify(mockUserDAO, times(1)).loginCheck(userCred.getUsername(), userCred.getPassword());
+        Assert.assertNotNull(actual);
+    }
+
+    @Test
+    public void test_loginUser_returnFalse_givenCredentialAreIncorrect() {
+        // Arrange
+        User newUser = new User("test", "123");
+        LoginCredentials userCred = new LoginCredentials();
+        userCred.setUsername("test");
+        userCred.setPassword("124");
+
+        // Act
+        when(mockUserDAO.loginCheck(userCred.getUsername(), userCred.getPassword())).thenReturn(null);
+        User actual = sut.loginUser(userCred);
+
+        //Assert
+        verify(mockUserDAO, times(1)).loginCheck(userCred.getUsername(), userCred.getPassword());
+        Assert.assertNull(actual);
+    }
 }
